902d4b682e994fd6ede6fea26f122625d689b038
alanbruno1994 <bruno47rios@outlook.com>
2022-05-01 17:26:49 -0300

    teste

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log('teste');
   await app.listen(3000);
 }
 bootstrap();
